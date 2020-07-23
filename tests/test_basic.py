0591568e5a86513f8f595238d8998e9bd7720352
Ninad Mhatre <ninad.mhatre@gmail.com>
2020-07-24 01:06:56 +0200

    Feature/nmhatre/0.3.0 (#8)
    * corrected links in setup.py
    
    * added license from original library
    
    * v0.3.0 : changed copy-paste code with own implementation and removed all copied code

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,7 +1,9 @@
 import unittest
-import os
+from contextlib import redirect_stderr
+from io import StringIO
 
-from simple_assertions import assert_that, PYASSERT_ERRORS_AS_WARNINGS
+from simple_assertions import check, PYASSERT_ERRORS_AS_WARNINGS, WarnVals
+from tests.helper import SetEnvVarContext
 
 
 class SampleMsg:
@@ -17,12 +19,12 @@ class SampleMsg:
 
 
 def assert_msg_type(msg):
-    assert_that(msg.msg_type, "msg_type").is_instance_of(str)
-    assert_that(msg.msg_type, "msg_type").is_in(("fix4.1", "fix4.2"))
+    check(msg.msg_type, "msg_type").is_instance_of(str)
+    check(msg.msg_type, "msg_type").is_in(("fix4.1", "fix4.2"))
 
 
 def assert_seq_num(msg):
-    assert_that(msg.seq_num, "seq_num").is_instance_of(int).is_equal_to(10)
+    check(msg.seq_num, "seq_num").is_instance_of(int).is_equal_to(10)
 
     # deliberately using bare assert
     assert msg.seq_num >= 10, "Expected: <{}> to be >= 10".format(msg.seq_num)
@@ -33,81 +35,89 @@ class FuncBasedCases(unittest.TestCase):
         self.msg = SampleMsg("fix4.1", 10, "trade_msg")
 
     def test_happy_path(self):
-        assert_that(self.msg.msg_type, "msg_type").is_equal_to("fix4.1")
-        assert_that(self.msg.seq_num, "seq_num").is_equal_to(10)
-        assert_that(self.msg.data, "data").is_populated()
+        check(self.msg.msg_type, "msg_type").is_equal_to("fix4.1")
+        check(self.msg.seq_num, "seq_num").is_equal_to(10)
+        check(self.msg.data, "data").is_populated()
 
         # chain
-        assert_that(self.msg.data, "data").is_populated().is_not_equal_to(
-            "any"
-        )
+        check(self.msg.data, "data").is_populated().is_not_equal_to("any")
 
     def test_basic_all_as_warn_using_arg(self):
         # all assertions will fail but still it will continue
-        assert_that(self.msg.msg_type, "msg_type", as_warn=True).is_equal_to(
-            "fix4"
-        )
-        assert_that(self.msg.seq_num, "seq_num", as_warn=True).is_equal_to(9)
-        assert_that(self.msg.data, "data", as_warn=True).is_not_populated()
+        check(self.msg.msg_type, "msg_type", as_warn=True).is_equal_to("fix4")
+        check(self.msg.seq_num, "seq_num", as_warn=True).is_equal_to(9)
+        check(self.msg.data, "data", as_warn=True).is_not_populated()
 
-    def test_basic_all_as_warn_env_var(self):
-        os.environ[PYASSERT_ERRORS_AS_WARNINGS] = "true"
+    def test_basic_all_as_warn_env_var_show_lineno(self):
+        with SetEnvVarContext(
+            PYASSERT_ERRORS_AS_WARNINGS, WarnVals.OnlyLineNum
+        ):
+            check(self.msg, "msg_type_check").is_not_instance_of(
+                (SampleMsg, int)
+            )
+            check(self.msg.msg_type, "msg_type").is_equal_to("fix4")
+            check(self.msg.seq_num, "seq_num").is_equal_to(9)
+            check(self.msg.data, "data").is_not_populated()
 
-        assert_that(self.msg, "mst_type_check").is_not_instance_of(
-            (SampleMsg, int)
-        )
-        assert_that(self.msg.msg_type, "msg_type").is_equal_to("fix4")
-        assert_that(self.msg.seq_num, "seq_num").is_equal_to(9)
-        assert_that(self.msg.data, "data").is_not_populated()
+    def test_basic_all_as_warn_env_var_traceback(self):
+        with SetEnvVarContext(PYASSERT_ERRORS_AS_WARNINGS, WarnVals.TraceBack):
+            output = StringIO()
+            with redirect_stderr(output):
+                check(self.msg, "msg_type_check").is_not_instance_of(
+                    (SampleMsg, int)
+                )
 
-        del os.environ[PYASSERT_ERRORS_AS_WARNINGS]
+            self.assertIn(
+                "traceback.print_stack()",
+                output.getvalue(),
+                "failed to find err text stderr! Captured:\n {}".format(output.getvalue()),
+            )
 
     def test_base_selective_warn(self):
-        assert_that(self.msg.msg_type, "msg_type").is_equal_to("fix4.1")
-        assert_that(self.msg.seq_num, "seq_num", as_warn=True).is_equal_to(5)
-        assert_that(self.msg.data, "data").is_populated()
+        check(self.msg.msg_type, "msg_type").is_equal_to("fix4.1")
+        check(self.msg.seq_num, "seq_num", as_warn=True).is_equal_to(5)
+        check(self.msg.data, "data").is_populated()
 
     def test_assert_in_multiple_places(self):
         assert_msg_type(self.msg)
         assert_seq_num(self.msg)
-        assert_that(self.msg.data, "data").is_populated().is_equal_to(
-            "trade_msg"
-        )
+        check(self.msg.data, "data").is_populated().is_equal_to("trade_msg")
 
     def test_raises_exceptions(self):
         with self.assertRaises(AssertionError):
-            assert_that(self.msg.msg_type).is_equal_to("not_matching")
+            check(self.msg.msg_type).is_equal_to("not_matching")
 
     def test_other_assertions(self):
-        assert_that("1").is_numeric()
-        assert_that(True, "there_will_be_cure_for_covid-19").is_true()
-        assert_that(False, "is_world_a_happy_place").is_false()
-        assert_that(1, "eq_or_in?").is_equal_or_in_seq(1)
-        assert_that(1, "eq_or_in?").is_equal_or_in_seq([1, 2, 3])
-        assert_that(1).is_not_instance_of(str)
-        assert_that(1).is_not_in([2, 3, 4])
+        check("1").is_numeric()
+        check(True, "there_will_be_cure_for_covid-19").is_true()
+        check(False, "is_world_a_happy_place").is_false()
+        check(1, "eq_or_in?").is_equal_or_in_seq(1)
+        check(1, "eq_or_in?").is_equal_or_in_seq([1, 2, 3])
+        check(1).is_not_instance_of(str)
+        check(1).is_not_in([2, 3, 4])
 
     def test_failed_assertions(self):
         with self.assertRaises(AssertionError):
-            assert_that("a").is_numeric()
+            check("a").is_numeric()
 
         with self.assertRaises(AssertionError):
-            assert_that("1").is_in([1, 2])
+            check("1").is_in([1, 2])
 
         with self.assertRaises(AssertionError):
-            assert_that(1).is_not_in([1, 2])
-            
-        with self.assertRaises(AssertionError):
-            assert_that(1).is_equal_or_in_seq([3, 4])
-            
-        with self.assertRaises(AssertionError):
-            assert_that(None).is_true()
+            check(1).is_not_in([1, 2])
 
         with self.assertRaises(AssertionError):
-            assert_that(1).is_false()
-            
+            check(1).is_equal_or_in_seq([3, 4])
+
         with self.assertRaises(AssertionError):
-            assert_that(1).is_instance_of(str)
+            check(None).is_true()
+
+        with self.assertRaises(AssertionError):
+            check(1).is_false()
+
+        with self.assertRaises(AssertionError):
+            check(1).is_instance_of(str)
+
 
 if __name__ == "__main__":
     unittest.main()
--- tests/test_composition.py
+++ tests/test_composition.py
@@ -1,7 +1,13 @@
 import unittest
-import os
+from contextlib import redirect_stderr
+from io import StringIO
 
-from simple_assertions import SimpleAssertions, PYASSERT_ERRORS_AS_WARNINGS
+from simple_assertions import (
+    SimpleAssertions,
+    PYASSERT_ERRORS_AS_WARNINGS,
+    WarnVals,
+)
+from tests.helper import SetEnvVarContext
 
 
 class SampleMsg:
@@ -16,9 +22,9 @@ class SampleMsg:
         )
 
 
-def assert_msg_type(assert_that, msg):
-    assert_that(msg.msg_type, "msg_type").is_instance_of(str)
-    assert_that(msg.msg_type, "msg_type").is_in(("fix4.1", "fix4.2"))
+def assert_msg_type(check, msg):
+    check(msg.msg_type, "msg_type").is_instance_of(str)
+    check(msg.msg_type, "msg_type").is_in(("fix4.1", "fix4.2"))
 
 
 class CompositionBasedUse(unittest.TestCase):
@@ -29,10 +35,10 @@ class CompositionBasedUse(unittest.TestCase):
 
     def setUp(self) -> None:
         self.msg = SampleMsg("fix4.1", 10, "trade_msg")
-        self.assert_that = SimpleAssertions().assert_that
+        self.check = SimpleAssertions().check
 
     def assert_seq_num(self):
-        self.assert_that(self.msg.seq_num, "seq_num").is_instance_of(
+        self.check(self.msg.seq_num, "seq_num").is_instance_of(
             int
         ).is_equal_to(10)
 
@@ -42,89 +48,92 @@ class CompositionBasedUse(unittest.TestCase):
         )
 
     def test_happy_path(self):
-        self.assert_that(self.msg.msg_type, "msg_type").is_equal_to("fix4.1")
-        self.assert_that(self.msg.seq_num, "seq_num").is_equal_to(10)
-        self.assert_that(self.msg.data, "data").is_populated()
+        self.check(self.msg.msg_type, "msg_type").is_equal_to("fix4.1")
+        self.check(self.msg.seq_num, "seq_num").is_equal_to(10)
+        self.check(self.msg.data, "data").is_populated()
 
         # chain
-        self.assert_that(self.msg.data, "data").is_populated().is_not_equal_to(
-            "any"
-        )
+        self.check(self.msg.data, "data").is_populated().is_not_equal_to("any")
 
     def test_basic_all_as_warn_using_arg(self):
         # all assertions will fail but still it will continue
-        self.assert_that(
-            self.msg.msg_type, "msg_type", as_warn=True
-        ).is_equal_to("fix4")
-
-        self.assert_that(
-            self.msg.seq_num, "seq_num", as_warn=True
-        ).is_equal_to(9)
-
-        self.assert_that(
-            self.msg.data, "data", as_warn=True
-        ).is_not_populated()
-
-    def test_basic_all_as_warn_env_var(self):
-        os.environ[PYASSERT_ERRORS_AS_WARNINGS] = "true"
-
-        self.assert_that(self.msg, "mst_type_check").is_not_instance_of(
-            (SampleMsg, int)
+        self.check(self.msg.msg_type, "msg_type", as_warn=True).is_equal_to(
+            "fix4"
         )
-        self.assert_that(self.msg.msg_type, "msg_type").is_equal_to("fix4")
-        self.assert_that(self.msg.seq_num, "seq_num").is_equal_to(9)
-        self.assert_that(self.msg.data, "data").is_not_populated()
 
-        del os.environ[PYASSERT_ERRORS_AS_WARNINGS]
+        self.check(self.msg.seq_num, "seq_num", as_warn=True).is_equal_to(9)
+
+        self.check(self.msg.data, "data", as_warn=True).is_not_populated()
+
+    def test_basic_all_as_warn_env_var_show_lineno(self):
+        with SetEnvVarContext(PYASSERT_ERRORS_AS_WARNINGS, WarnVals.OnlyLineNum):
+            self.check(self.msg, "mst_type_check").is_not_instance_of(
+                (SampleMsg, int)
+            )
+            self.check(self.msg.msg_type, "msg_type").is_equal_to("fix4")
+            self.check(self.msg.seq_num, "seq_num").is_equal_to(9)
+            self.check(self.msg.data, "data").is_not_populated()
+
+    def test_basic_all_as_warn_env_var_traceback(self):
+        with SetEnvVarContext(PYASSERT_ERRORS_AS_WARNINGS, WarnVals.TraceBack):
+            output = StringIO()
+            with redirect_stderr(output):
+                self.check(self.msg, "msg_type_check").is_not_instance_of(
+                    (SampleMsg, int)
+                )
+            self.assertIn(
+                "traceback.print_stack()",
+                output.getvalue(),
+                "failed to find err text stderr",
+            )
 
     def test_base_selective_warn(self):
-        self.assert_that(self.msg.msg_type, "msg_type").is_equal_to("fix4.1")
-        self.assert_that(
-            self.msg.seq_num, "seq_num", as_warn=True
-        ).is_equal_to(5)
-        self.assert_that(self.msg.data, "data").is_populated()
+        self.check(self.msg.msg_type, "msg_type").is_equal_to("fix4.1")
+        self.check(self.msg.seq_num, "seq_num", as_warn=True).is_equal_to(5)
+        self.check(self.msg.data, "data").is_populated()
 
     def test_assert_in_multiple_places(self):
-        assert_msg_type(self.assert_that, self.msg)
+        assert_msg_type(self.check, self.msg)
         self.assert_seq_num()
-        self.assert_that(self.msg.data, "data").is_populated().is_equal_to(
+        self.check(self.msg.data, "data").is_populated().is_equal_to(
             "trade_msg"
         )
 
     def test_raises_exceptions(self):
         with self.assertRaises(AssertionError):
-            self.assert_that(self.msg.msg_type).is_equal_to("not_matching")
+            self.check(self.msg.msg_type).is_equal_to("not_matching")
 
     def test_other_assertions(self):
-        self.assert_that("1").is_numeric()
-        self.assert_that(True, "there_will_be_cure_for_covid-19").is_true()
-        self.assert_that(False, "is_world_a_happy_place").is_false()
-        self.assert_that(1, "eq_or_in?").is_equal_or_in_seq(1)
-        self.assert_that(1, "eq_or_in?").is_equal_or_in_seq([1, 2, 3])
-        self.assert_that(1).is_not_instance_of(str)
-        self.assert_that(1).is_not_in([2, 3, 4])
+        self.check("1").is_numeric()
+        self.check(True, "there_will_be_cure_for_covid-19").is_true()
+        self.check(False, "is_world_a_happy_place").is_false()
+        self.check(1, "eq_or_in?").is_equal_or_in_seq(1)
+        self.check(1, "eq_or_in?").is_equal_or_in_seq([1, 2, 3])
+        self.check(1).is_not_instance_of(str)
+        self.check(1).is_not_in([2, 3, 4])
 
     def test_failed_assertions(self):
         with self.assertRaises(AssertionError):
-            self.assert_that("a").is_numeric()
+            self.check("a").is_numeric()
 
         with self.assertRaises(AssertionError):
-            self.assert_that("1").is_in([1, 2])
+            self.check("1").is_in([1, 2])
 
         with self.assertRaises(AssertionError):
-            self.assert_that(1).is_not_in([1, 2])
-            
-        with self.assertRaises(AssertionError):
-            self.assert_that(1).is_equal_or_in_seq([3, 4])
-            
-        with self.assertRaises(AssertionError):
-            self.assert_that(None).is_true()
+            self.check(1).is_not_in([1, 2])
 
         with self.assertRaises(AssertionError):
-            self.assert_that(1).is_false()
-            
+            self.check(1).is_equal_or_in_seq([3, 4])
+
         with self.assertRaises(AssertionError):
-            self.assert_that(1).is_instance_of(str)
+            self.check(None).is_true()
+
+        with self.assertRaises(AssertionError):
+            self.check(1).is_false()
+
+        with self.assertRaises(AssertionError):
+            self.check(1).is_instance_of(str)
+
 
 if __name__ == "__main__":
     unittest.main()
